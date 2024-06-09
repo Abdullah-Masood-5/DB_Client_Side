5dd803bb62db20bf3aded327f76b9fed3c3d986f
BazilSuhail <bazil1854@gmail.com>
2024-06-10 03:36:05 +0500

    Added all necessary css , and resolved all error in App.js as well as transaction_history's file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 
 import "../src/Components/Styles/loader.css";
+import Loader from "./Components/Loader";
+import AddDummyDataToFirestore from "./Components/Insertdata";  
 // Lazy loading components
 const Home = React.lazy(() => import("./Components/Home"));
-const SignUp = React.lazy(() => import("./Components/Signup"));
-const Login = React.lazy(() => import("./Components/Login"));
 const Donor = React.lazy(() => import("./Components/Donor"));
+const Login = React.lazy(() => import("./Components/Login"));
+const SignUp = React.lazy(() => import("./Components/Signup"));
+const Gallery = React.lazy(() => import("./Components/Gallery"));
+const Volunteer = React.lazy(() => import("./Components/Volunteer"));
 const Listedprojects = React.lazy(() => import("./Components/Projects_list"));
 const AppliedProj = React.lazy(() => import("./Components/Applied_projects"));
-const Listcampaigns = React.lazy(() => import("./Components/Campaigns_list")); 
-const Testimonial = React.lazy(() => import("./Components/Testimonial"));
-
-
-const Volunteer = React.lazy(() => import("./Components/Volunteer"));
-
-
-
-const TransactionHistory = React.lazy(() => import("./Components/Transaction_History")); 
+const TransactionHistory = React.lazy(() => import("./Components/Transaction_History"));
+const Listcampaigns = React.lazy(() => import("./Components/Campaigns_list"));
 const Complains = React.lazy(() => import("./Components/Complains"));
-
-const Gallery = React.lazy(() => import("./Components/Gallery")); 
+const Testimonial = React.lazy(() => import("./Components/Testimonial"));
+ 
 
 const App = () => {
 
@@ -29,27 +26,30 @@ const App = () => {
     <Router>
 
       <Navbar />
-      <Suspense fallback={<>d</> }>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/donor" element={<Donor />} />
-        <Route path="/volunteer" element={<Volunteer />} /> 
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/transactionhistory" element={<TransactionHistory />} />
+      <Suspense fallback={<Loader typeOfloader="skeleton" />}>
 
+        <Routes>
+          {/*  <Route path="/" element={<Loader typeOfloader="a" />} />*/}
+          <Route path="/" element={<Home />} />
+          <Route path="/donor" element={<Donor />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/listedprojects" element={<Listedprojects />} />
+          <Route path="/listcampaigns" element={<Listcampaigns />} />
+          <Route path="/AppliedProject" element={<AppliedProj />} />
+          <Route path="/transactionhistory" element={<TransactionHistory />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/donor" element={<Donor />} />
+          <Route path="/volunteer" element={<Volunteer />} /> 
+          <Route path="/complains" element={<Complains/>} /> 
+          <Route path="/testimonial" element={<Testimonial/>} />  
 
-        <Route path="/listedprojects" element={<Listedprojects />} />
-        <Route path="/listcampaigns" element={<Listcampaigns />} />
-        <Route path="/AppliedProject" element={<AppliedProj />} />
-        <Route path="/testimonial" element={<Testimonial/>} />  
-        
-        <Route path="/complains" element={<Complains/>} />  
-      </Routes>
+          
+          <Route exact path="/dummy" element={<AddDummyDataToFirestore />} />
+        </Routes>
+
 
       </Suspense>
-
     </Router>
   );
 };
